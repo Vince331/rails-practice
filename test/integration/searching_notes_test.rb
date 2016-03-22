18ec67c9e4e4202b5f2dcb4955e96a7be5627d79
Vince Lundgren <vincent.lundgren@gmail.com>
2016-03-22 09:43:20 -0500

    add a test for searching notes, wire up capybara

---
+++ test/integration/searching_notes_test.rb
@@ -0,0 +1,15 @@
+require 'test_helper'
+
+class SearchingNotesTest < ActionDispatch::IntegrationTest
+  def test_i_can_search_for_notes
+  page.visit "/"
+   # i go to "/"
+   # and I see a form that asks me to serach my notes
+   # I enter an array
+   # i submit the form
+   # my url says "/notes?query=arr"
+   # and i see my notes that include "arr"
+   # and i dont see notes that i didnt serach for
+
+  end
+end
