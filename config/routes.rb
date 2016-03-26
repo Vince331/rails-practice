2264535fc5e7abf838617d78d39fbc243ab32c9e
Vince Lundgren <vincent.lundgren@gmail.com>
2016-03-26 16:16:13 -0500

    added styling and heroku

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,7 @@ Rails.application.routes.draw do
   # Example of regular route:
      root 'notes#search'
      get "about" => "pages#about" #creates about_path
+     get "find"  => "pages#find" #creates find_path
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
